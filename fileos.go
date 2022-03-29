04409cc86892e660f97b6bce68f34e00145de499
xuguoqiang <xuguoqiang@weimiao.cn>
2022-03-29 22:42:45 +0800

    akk

---
+++ fileos.go
@@ -0,0 +1,17 @@
+package fileos
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func del(a int, b int) int {
+	return a - b
+}
+
+func mul(a int, b int) int {
+	return a * b
+}
+
+func shang(a int, b int) int {
+	return a / b
+}
